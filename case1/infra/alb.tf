05f1753e7e250dc3f169987c623740e3601c89dd
iamprasad <durgaprasadkalepuu@gmail.com>
2026-04-14 14:07:29 +0530

    Add AWS ALB and related resources configuration

---
+++ case1/infra/alb.tf
@@ -0,0 +1,31 @@
+resource "aws_lb" "main" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.public[*].id
+}
+
+resource "aws_lb_target_group" "app" {
+  name        = "app-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path = "/health" # Matching your index.js
+    port = "8080"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
