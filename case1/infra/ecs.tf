f6dcb9a3294cf39879dc3b9f5fc4d9aaac095682
iamprasad <durgaprasadkalepuu@gmail.com>
2026-04-14 14:10:20 +0530

    Add output for ECR repository URL
    Add output for ECR repository URL.

--- case1/infra/ecs.tf
+++ case1/infra/ecs.tf
@@ -57,3 +57,6 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
